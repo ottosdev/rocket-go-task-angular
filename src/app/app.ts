a3787a885d637a47c790f4ebea335927a5ad2389
Otto de Souza <osouza@minsit.com>
2025-10-06 21:30:34 -0300

    main content and task list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
+import { MainContent } from './shared/components/main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, MainContent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('rocket-go-task');
